1f3024f624c742b9b4677f4de47906a1be5c42f6
Acengen <kocic999@yahoo.com>
2020-02-21 13:14:03 +0100

    change

---
+++ app.js
@@ -0,0 +1,66 @@
+let min =1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+ 
+const subBtn = document.querySelector('#guess-value');
+const msg = document.querySelector('.message');
+const game = document.querySelector('.game');
+
+
+function clearFields() {
+    document.querySelector('.guess-input').value = '';
+}
+function lose() {
+    if(guessesLeft <= 0) {
+        msg.innerHTML = `Game over u lose!`;
+        subBtn.value = 'Play again';
+        subBtn.className += 'play-again';
+        return;
+    }
+}
+function won(input) {
+    if(input === winningNum) {
+        const border = document.querySelector('.guess-input');
+        border.classList.remove('lose');
+        border.classList.add('win');
+        msg.innerHTML = 'Game over u Win';
+        msg.classList.remove('text-lose');
+        msg.classList.add('text-won');
+        subBtn.value = 'Play again';
+        subBtn.className += 'play-again';
+        return;
+    }
+}
+subBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    const input = parseFloat(document.querySelector('.guess-input').value);
+    
+    if(input > 10 || input < 1) {
+        msg.innerHTML = 'Please enter numbers between 1 and 10';
+        msg.classList.add('text-lose');
+        clearFields();
+        return;
+    }
+    if(input !== winningNum) {
+        guessesLeft--;
+        const border = document.querySelector('.guess-input');
+        border.classList.add('lose');
+        msg.innerHTML = `u have ${guessesLeft} more guess left`;
+        msg.classList.add('text-lose');
+        lose();
+    }
+    else {
+        won(input);
+    }
+
+    
+    clearFields();
+});
+
+game.addEventListener('mousedown', e => {
+    if(e.target.className === 'play-again') {
+        window.location.reload();
+    }
+})
